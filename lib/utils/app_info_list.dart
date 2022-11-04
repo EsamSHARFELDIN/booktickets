893da3a82662bc45fc1b8f0846fae115440ff99c
Esam sharfeldin <esamsharf2@gmail.com>
2022-11-04 14:43:09 +0100

    add: the dynamic for hotels info

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,20 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotelRoom1.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'hotelRoom2.png',
+    'place': 'Global Will',
+    'destination': 'Port Sudan',
+    'price': 40
+  },
+  {
+    'image': 'hotelRoom3.png',
+    'place': 'Tallest Building',
+    'destination': 'Khartoum',
+    'price': 68
+  },
+];

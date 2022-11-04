e8b85329045fe5b574ac11c19722f37906787fa1
Esam sharfeldin <esamsharf2@gmail.com>
2022-11-04 14:43:09 +0100

    add: the dynamic for hotels info

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:booktickets/screens/hotel_screen.dart';
 import 'package:booktickets/screens/ticket_view.dart';
+import 'package:booktickets/utils/app_info_list.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
@@ -98,7 +100,48 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           const Gap(15),
-          TicketView(),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(left: 20),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: ticketList
+                  .map((singleTicket) => TicketView(ticket: singleTicket))
+                  .toList(),
+            ),
+          ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Hotels",
+                  style: Styles.headLineStyle2,
+                ),
+                InkWell(
+                  onTap: () {
+                    print("You get tap !");
+                  },
+                  child: Text(
+                    "View all",
+                    style:
+                        Styles.textStyle.copyWith(color: Styles.primaryColor),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(left: 20),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: hotelList
+                  .map((singlehotel) => HotelScreen(hotel: singlehotel))
+                  .toList(),
+            ),
+          )
         ],
       ),
     );
---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,47 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotelRoom1.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'hotelRoom2.png',
+    'place': 'Global Will',
+    'destination': 'Port Sudan',
+    'price': 40
+  },
+  {
+    'image': 'hotelRoom3.png',
+    'place': 'Tallest Building',
+    'destination': 'Khartoum',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "DK", 'name': "Dhaka"},
+    'to': {'code': "KH", 'name': "Khartoum"},
+    'flying_time': '3H 30M',
+    'date': "1 MAY",
+    'departure_time': "17:30 AM",
+    "number": 249
+  },
+  {
+    'from': {'code': "NYC", 'name': "New-Yourk"},
+    'to': {'code': "LDN", 'name': "London"},
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23
+  },
+  {
+    'from': {'code': "SD", 'name': "Khartoum"},
+    'to': {'code': "SH", 'name': "Shanghai"},
+    'flying_time': '4H 20M',
+    'date': "1 MAY",
+    'departure_time': "09:00 AM",
+    "number": 45
+  },
+];
